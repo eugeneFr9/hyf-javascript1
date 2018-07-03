e23be58eeb99b2f2744b3a44e15882c6035e7917
Evgenii <39793088+genegr88@users.noreply.github.com>
2018-07-03 19:10:52 +0200

    Update exercise4.js

--- week1/exercise4.js
+++ week1/exercise4.js
@@ -3,4 +3,4 @@ console.log("the value of my string will be: What the hell?");
 console.log(y);
 y = "No problem";
 console.log("the value of my string will be: No problem");
-console.log(y);
+console.log(y)
