924e6d498fc6b0379715e788db7b5f8ac3918b8b
Evgenii <39793088+genegr88@users.noreply.github.com>
2018-07-03 18:37:16 +0200

    Delete exercise6.js

--- exercise6.js
+++
@@ -1,7 +0,0 @@
-ver myArray = [];
-console.log("the value of the array is []);
-console.log(myArray);
-var favAnimals = ["cat", "dog", "horse", "rabbit", "fox"];
-console.log(favAnimals);
-favAnimals.push("baby pig");
-console.log(favAnimals);
