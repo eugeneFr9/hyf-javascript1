ca7707305773928e2e0f014787b29dddd60f7f7e
Evgenii <39793088+genegr88@users.noreply.github.com>
2018-07-03 18:31:41 +0200

    Delete exercise4.js

--- exercise4.js
+++
@@ -1,6 +0,0 @@
-var y = "What the hell?";
-console.log("the value of my string will be: What the hell?");
-console.log(y);
-y = "No problem";
-console.log("the value of my string will be: No problem");
-console.log(y);
