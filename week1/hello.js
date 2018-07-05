53b6af3fdcb4865f421570a1997eea230a8a853b
Evgenii <39793088+genegr88@users.noreply.github.com>
2018-07-05 13:19:22 +0200

    added "Hello" file

---
+++ week1/hello.js
@@ -0,0 +1,4 @@
+var class16 = ["Said", "Evgenii", "Donald", "Marwan", "Ward", "Andrew", "Ernesto", "Sam", "Sima", "Shadi", "Karam", "Salar", "Barbaros", "Gaffari"]
+for (i = 0; i < class16.length; i++) {
+console.log("Hello, " + class16[i]);
+}
